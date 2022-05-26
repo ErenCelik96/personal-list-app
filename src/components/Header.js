ab3ea6fa016e98296833abdb4f97a5f47a6019dd
Erencan ÇELİK <celik.erenn96@gmail.com>
2022-05-27 00:54:18 +0300

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import { Space, Typography } from 'antd'
+import React from 'react'
+import Logo from '../assets/images/green-logo.png'
+
+const { Title } = Typography;
+
+const Header = () => {
+    return (
+        <Space style={{ height: '80px', backgroundColor: '#2CC44D', boxShadow: '3px 3px 3px 3px gray', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <img width="160px" src={Logo} />
+            <Title level={1} style={{color:'white'}}> Staff of the Month List</Title>
+        </Space>
+    )
+}
+
+export default Header
